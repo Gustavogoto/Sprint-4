8fe1cb376fcdf1739092e1832cd7e5eb37d8399e
Gustavo Akio <gustavoakio27@gmail.com>
2023-09-18 02:33:14 +0100

    Projeto finalizado

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Contato from './componentes/contato/Contato'
+import Footer from './componentes/footer/Footer'
+import Home from './componentes/home/Home'
+import Quem_somos from './componentes/quem _somos/Quem_somos'
+import ColorSchemesExample from './componentes/nav/Nav'
+import Carousels from './componentes/carousels/Carousels'
+import Como_funciona from './componentes/como_funciona/Como_funciona'
+import Body from './componentes/Body/body'
+import Calculo from './componentes/calculo/Calculo'
+import Numero from './componentes/Numero/Numero'
+
+
+
+function App() {
+
+  return (
+    <div className='App'>
+        <BrowserRouter>
+          <ColorSchemesExample/>
+            <Routes>
+              <Route path='/' element = {<Home/>}/>
+              <Route path='/contato' element = {<Contato/>}></Route>
+              <Route path='/footer' element = {<Footer/>}></Route>
+              <Route path='/carousels' element = {<Carousels/>}></Route>
+              <Route path='/Como_funciona' element = {<Como_funciona/>}></Route>
+              <Route path='/quem_somos' element = {<Quem_somos/>}></Route>
+              <Route path='/calculo' element = {<Calculo/>}></Route>
+              <Route path='/Numero' element = {<Numero/>}></Route>
+              <Route path='/Body' element = {<Body/>}></Route>
+            </Routes>
+        </BrowserRouter>
+        <Numero/>
+        <Footer/>
+        
+      </div>
+  )
+}
+
+export default App
+
+
